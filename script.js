a7f02a2886be6aeaaf1df86b584ec8eb14c10e70
JoKurio <jokurio6@gmail.com>
2019-09-04 04:37:43 +0100

    Add a 'contact me' form to resume

---
+++ script.js
@@ -0,0 +1,23 @@
+function validate() {
+      
+    if( document.contactMe.name.value == "" ||
+    document.contactMe.name.value.length < 4 ){
+       alert( "Please enter your full name!" );
+       document.contactMe.name.focus() ;
+       return false;
+    }
+    if( document.contactMe.email.value == "") {
+       alert( "Please enter a valid Email address!" );
+       document.contactMe.email.focus() ;
+       return false;
+    }
+    if( document.contactMe.message.value == ""  ||
+       document.contactMe.message.value.length < 20 ) {
+       
+       alert( "Please provide more details." );
+       document.contactMe.message.focus() ;
+       return false;
+    }
+    return( true ) && ( "Thank You!" );
+    
+ }
